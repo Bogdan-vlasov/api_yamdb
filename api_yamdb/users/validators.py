aae166bd6980d398ec83c8aa3abe1eb6655dff64
Sprngtm4htlr <avraamchichikov@yandex.ru>
2022-09-18 02:24:28 +0300

    first commit

--- api_yamdb/users/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def username_validator(value):
-    if value == 'me':
-        raise ValidationError(
-            'Запрещено использование имени пользователя "me".',
-            params={'value': value},
-        )
